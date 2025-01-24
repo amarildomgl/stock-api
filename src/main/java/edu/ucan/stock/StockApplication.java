2ce0ab369a0b1ad016b91f0a873e5f90f5e11968
Amarildo Miguel <amarildomgl@gmail.com>
2025-01-24 22:51:52 +0100

    fix: correção da versão do spring para java 17

--- src/main/java/edu/ucan/stock/StockApplication.java
+++ src/main/java/edu/ucan/stock/StockApplication.java
@@ -3,14 +3,14 @@ package edu.ucan.stock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCaching
 public class StockApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StockApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(StockApplication.class, args);
+    }
 
 }
